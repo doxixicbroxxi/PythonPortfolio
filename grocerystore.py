4d0da96b60a50af4b12a9f7b87418b4077a0e14d
doxixicbroxxi <dbrogni@cps.edu>
2026-05-27 09:01:56 -0500

    Add files via upload

---
+++ grocerystore.py
@@ -0,0 +1,81 @@
+#Dominic Brogni
+#Grocery Store
+#Code inventory & prices of items at a grocery store
+#Init
+items = [
+    "Milk", "Eggs", "Bread", "Butter", "Cheese",
+    "Chicken Breast", "Ground Beef", "Pork Chops", "Bacon", "Sausage",
+    "Pasta", "Rice", "Quinoa", "Cereal", "Oatmeal",
+    "Apples", "Bananas", "Oranges", "Strawberries", "Blueberries",
+    "Grapes", "Pineapple", "Mangoes", "Lemons", "Limes",
+    "Potatoes", "Onions", "Garlic", "Carrots", "Broccoli",
+    "Cauliflower", "Spinach", "Kale", "Lettuce", "Bell Peppers",
+    "Tomatoes", "Cucumbers", "Zucchini", "Mushrooms", "Avocados",
+    "Olive Oil", "Vegetable Oil", "Cooking Spray", "Salt", "Pepper",
+    "Sugar", "Brown Sugar", "Flour", "Baking Powder", "Baking Soda",
+    "Peanut Butter", "Jelly", "Honey", "Maple Syrup", "Nutella",
+    "Coffee", "Tea", "Hot Chocolate", "Creamer", "Sugar Packets",
+    "Orange Juice", "Apple Juice", "Soda", "Sparkling Water", "Sports Drink",
+    "Chips", "Crackers", "Pretzels", "Popcorn", "Cookies",
+    "Ice Cream", "Frozen Pizza", "Frozen Vegetables", "Frozen Chicken Nuggets", "Frozen Waffles",
+    "Canned Soup", "Canned Beans", "Canned Corn", "Canned Tomatoes", "Canned Tuna",
+    "Ketchup", "Mustard", "Mayonnaise", "BBQ Sauce", "Hot Sauce",
+    "Paper Towels", "Toilet Paper", "Facial Tissues", "Dish Soap", "Laundry Detergent"
+] #Tracks items
+prices = [
+
+    3.49, 2.99, 2.79, 3.99, 4.59,
+    7.99, 6.49, 5.99, 4.99, 4.49,
+    1.79, 4.29, 5.99, 3.99, 3.49,
+    4.49, 1.39, 3.99, 4.99, 5.49,
+    5.49, 3.49, 4.99, 1.79, 1.79,
+    3.99, 2.49, 1.29, 2.29, 2.99,
+    3.49, 3.49, 3.99, 2.49, 3.49,
+    2.99, 1.79, 2.49, 2.99, 1.99,
+    7.99, 5.49, 3.99, 1.29, 2.49,
+    2.49, 2.99, 3.49, 2.49, 1.99,
+    3.99, 3.29, 4.99, 6.99, 5.49,
+    9.99, 4.49, 3.99, 2.49, 1.99,
+    4.29, 3.99, 1.99, 2.49, 2.99,
+    3.99, 3.49, 2.99, 2.49, 4.99,
+    5.99, 6.49, 2.99, 7.49, 4.99,
+    2.79, 1.49, 1.29, 1.79, 2.49,
+    2.99, 2.49, 4.49, 3.49, 3.99,
+    8.99, 12.99, 3.99, 2.99, 11.99
+] #Tracks prices
+less_than = []
+#Functions
+status="error"
+
+#Challenge 1
+def price_check(item_name): #Looks for price of given item
+    global status
+    for i in range(len(items)):
+        if item_name==items[i]:
+            status="found"
+            print(prices[i])
+    if status=="error":
+        print("Item not found!")
+    else:
+        status="error"
+
+#Challenge 2
+def discount(cut): #Applies a discount to items
+    for i in range(len(prices)):
+        prices[i]=round(prices[i]*(1-cut),2)
+
+#Challenge 3
+def price_match(price): #Finds any items that can be purchased with the given price
+    for i in range(len(prices)):
+        if prices[i]<=price:
+            less_than.append(items[i])
+
+#Main
+#Final Test
+discount(0.2)
+print(items)
+print(prices)
+price_match(2)
+print(less_than)
+print(round(prices[items.index("Frozen Waffles")]+prices[items.index("Coffee")]+prices[items.index("Sugar")]+prices[items.index("Maple Syrup")]+prices[items.index("Butter")],2))
+
